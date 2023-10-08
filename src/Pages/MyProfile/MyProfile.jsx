76488fa3d328246bdca0c9d3162de8884df6781a
Tiaguh <tiagosingup@gmail.com>
2023-10-07 23:03:19 -0300

    creating button to see tags and edit profile

--- src/Pages/MyProfile/MyProfile.jsx
+++ src/Pages/MyProfile/MyProfile.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { BsFillCameraFill } from 'react-icons/bs';
 import { BiSolidPencil } from 'react-icons/bi';
 import { FaUserAlt } from 'react-icons/fa';
-import { IoEllipsisHorizontal } from 'react-icons/io5';
+import { IoEllipsisHorizontal, IoAddOutline } from 'react-icons/io5';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import Header from '../../components/Header/Header'
 
-import { Container, WallPaper, UserContainer, UserInfo, UserPhoto, UserName, UserCity, UserActions, SeeFriends, EditProfile, TagsContainer, Tag, SeeMoreButton, ProfileFeed } from './Style.js'
+import { Container, WallPaper, UserContainer, UserInfo, UserPhoto, UserName, UserCity, UserActions, SeeFriends, EditProfile, TagsContainer, Tag, SeeMoreDesable, SeeMoreActivity, ProfileFeed, Option, Line, CloseSeeMore, Options, OptionText } from './Style.js'
 
 export default function MyProfile() {
+    const [visible, setVisible] = useState(false)
+
     return (
         <Container>
-            <Header />
+            <Header />Option
 
             <WallPaper />
 
@@ -39,7 +42,7 @@ export default function MyProfile() {
                     </SeeFriends>
 
                     <EditProfile>
-                        <BiSolidPencil color="#000" size={30} />
+                        <BiSolidPencil style={{ cursor: "pointer" }} color="#000" size={30} />
                     </EditProfile>
 
                 </UserActions>
@@ -49,9 +52,38 @@ export default function MyProfile() {
             <TagsContainer>
                 <Tag># Adicione seus esportes favoritos </Tag>
 
-                <SeeMoreButton>
-                    <IoEllipsisHorizontal color="#FFF" size={30} />
-                </SeeMoreButton>
+                {
+                    visible ? (
+                        <SeeMoreActivity onClick={()=> setVisible(false)} >
+
+                            <Options>
+
+                                <Option>
+                                    <BiSolidPencil style={{ cursor: "pointer" }} color="#FFF" size={30} />
+                                    <OptionText>Editar Tags</OptionText>
+                                </Option>
+
+                                <Line />
+
+                                <Option>
+                                    <IoAddOutline style={{ cursor: "pointer" }} color="#FFF" size={35} />
+                                    <OptionText>Editar Tags</OptionText>
+                                </Option>
+
+                            </Options>
+
+                            <CloseSeeMore>
+                                <AiOutlineClose style={{ cursor: "pointer" }} color="#FFF" size={30} />
+                            </CloseSeeMore>
+
+                        </SeeMoreActivity>
+                    ) : (
+                        <SeeMoreDesable onClick={()=> setVisible(true)} >
+                            <IoEllipsisHorizontal color="#FFF" size={30} />
+                        </SeeMoreDesable>
+                    )
+                }
+
             </TagsContainer>
 
             <ProfileFeed>
--- src/Pages/MyProfile/Style.js
+++ src/Pages/MyProfile/Style.js
@@ -38,7 +38,7 @@ export const UserInfo = styled.div`
     flex-direction: column;
 `;
 
-export const UserPhoto = styled.div`
+export const UserPhoto = styled.button`
     width: 190px;
     height: 190px;
 
@@ -54,6 +54,8 @@ export const UserPhoto = styled.div`
 
     margin-top: -100px;
     margin-bottom: 15px;    
+
+    cursor: pointer;
 `;
 
 export const UserName = styled.div`
@@ -96,7 +98,9 @@ export const SeeFriends = styled.button`
 
     font-family: sans-serif;
     font-size: 20px;
-    color: #FFF    
+    color: #FFF;
+
+    cursor: pointer;
 `
 
 export const EditProfile = styled.button`
@@ -107,7 +111,7 @@ export const EditProfile = styled.button`
 
 export const TagsContainer = styled.div`
     width: 100%;
-    height: 15vh;
+    height: 18vh;
 
     display: flex;
     justify-content: space-between;
@@ -140,7 +144,7 @@ export const Tag = styled.button`
     cursor: pointer;
 `;
 
-export const SeeMoreButton = styled.button`
+export const SeeMoreDesable = styled.button`
     width: 50px;
     height: 50px;
 
@@ -154,6 +158,68 @@ export const SeeMoreButton = styled.button`
     border-radius: 50px;
 
     cursor: pointer;
+`;
+
+export const SeeMoreActivity = styled.div`
+    width: 200px;
+    height: 100px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #F8670E;
+
+    border: none;
+    border-radius: 25px;
+
+    cursor: pointer;
+
+    padding-inline: 5px
+`;
+
+export const Options = styled.div`
+    width: 70%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+
+`
+
+export const Option = styled.div`
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    gap: 5px
+`;
+
+export const Line = styled.div`
+    width: 100%;
+    height: 2px;
+
+    background-color: #FFF;   
+`;
+
+export const CloseSeeMore = styled.button`
+    width: 25%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border: none;
+    background-color: transparent;
+`
+
+export const OptionText = styled.div`
+    font-family: sans-serif;
+    font-size: 17px;
+    color: #FFF;
 `;
 
 export const ProfileFeed = styled.div`
